c4388d1627e33338f1584819ce503f4535641ec7
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2023-04-12 21:58:51 -0700

    first commit

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a JSON file-writing function."""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """The function uses json representation to write an object to a text file."""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
