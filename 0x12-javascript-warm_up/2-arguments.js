4a924c3dc81d473813a53fb4cfe7c8791ab3080c
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2023-06-14 16:52:46 +0100

    commiting all files

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-if(process.argv.length === 2) {
-    console.log("No arguement");
-} else if(process.argv.length === 3) {
-    console.log("Arguement found");
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-    console.log("Arguements found")
+  console.log('Arguments found');
 }
