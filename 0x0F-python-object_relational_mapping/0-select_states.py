e405a1c0d27b2d289f96b71b35714b72f2574619
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2023-06-19 23:36:29 +0100

    commit all

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This  lists all the states from the database 
+It takes 3 arguements: mysql username, msql password, database name
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__"
+    db = MySQLdb.connect(host="localhost", user="argv[1]",
+                         passwd="argv[2]", db="argv[3]")
+    cursor = db.cursor9()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    for state in cursor.fetchall():
+        print(state)
+    cursor.close()
+    db.close()
