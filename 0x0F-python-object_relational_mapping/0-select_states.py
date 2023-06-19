863cb46846fe884aa6118b84b6da2be20acdceef
Ezeh Christian Chiedozie <edozievip898@gmail.com>
2023-06-19 23:41:43 +0100

    commit one

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-This  lists all the states from the database 
+This lists all the states from the database
 It takes 3 arguements: mysql username, msql password, database name
 """
 
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user="argv[1]",
                          passwd="argv[2]", db="argv[3]")
     cursor = db.cursor9()
